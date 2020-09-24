dfade7ae37784805a3c8440cfc3e5410b21c8723
Sasha <bychuk.sasha8@gmail.com>
2020-09-24 16:50:15 +0300

    Initial commit

---
+++ Dif.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int Dif (int a,int b)
+    return a-b;
+}
+int main()
+{
+	int a,b;
+	cin >> a >> b;
+	int  res = Dif(a,b);
+}
---
+++ Dob.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int Sum (int a,int b)
+    return a+b;
+}
+int main()
+{
+	int a,b;
+	cin >> a >> b;
+	int res = Sum(a,b);
+}
